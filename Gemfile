09802307239bd8b68f398d7aa4af09d3c54dc308
Troex Nevelin <troex@fury.scancode.ru>
2012-07-01 19:34:08 +0300

    initial

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :rubygems
+
+gem 'rails', '3.2.6'
+
+gem 'jquery-rails'
+
+gem 'unicorn'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simple::Application.config.secret_token = '6a63558e16ccaf193b36f61f3fa0abf55abbbf4a384f712033fe25fde1d8a50ffe8239d5f8fdb88821cde70d2c02b622fe9be9be9360c738384e281071644091'
